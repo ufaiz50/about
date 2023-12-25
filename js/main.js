7fe55c31928e7f7a5c79d60404ea9e44031a0306
Umar Faiz Ubaidillah <dev023@kppmining.com>
2023-12-25 14:46:37 +0700

    Refactoring::[Section About -> change()]

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 $(document).ready(function () {
   let $btns = $(".project-area .button-group button");
 
-  $btns.click(function (e) {
-    $(".project-area .button-group button").removeClass("active");
-    e.target.classList.add("active");
-    let selector = $(e.target).attr("data-filter");
-    $(".project-area .grid").isotope({
-      filter: selector,
-    });
-    return false;
-  });
+  // $btns.click(function (e) {
+  //   $(".project-area .button-group button").removeClass("active");
+  //   e.target.classList.add("active");
+  //   let selector = $(e.target).attr("data-filter");
+  //   $(".project-area .grid").isotope({
+  //     filter: selector,
+  //   });
+  //   return false;
+  // });
 
   $(".project-area .button-group #btn1").trigger("click");
 
-  $(".project-area .grid .test-popup-link").magnificPopup({
-    type: "image",
-    gallery: { enabled: true },
-  });
+  // $(".project-area .grid .test-popup-link").magnificPopup({
+  //   type: "image",
+  //   gallery: { enabled: true },
+  // });
 
   //owl-carousel
-  $(".site-main .about-area .owl-carousel").owlCarousel({
-    loop: true,
-    autoplay: true,
-    dots: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      544: {
-        items: 2,
-      },
-    },
-  });
+  // $(".site-main .about-area .owl-carousel").owlCarousel({
+  //   loop: true,
+  //   autoplay: true,
+  //   dots: true,
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //     },
+  //     544: {
+  //       items: 2,
+  //     },
+  //   },
+  // });
 
   // sticky navigation menu
 
